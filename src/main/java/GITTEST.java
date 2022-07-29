a0eb37c127e7d6721768e8965db72bd8e998d813
gz <1462677477@qq.com>
2022-07-29 16:26:31 +0800

    Second Commit

--- src/main/java/GITTEST.java
+++ src/main/java/GITTEST.java
@@ -2,5 +2,6 @@ public class GITTEST {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello,hello");
+        System.out.println("hello,hello");
     }
 }
